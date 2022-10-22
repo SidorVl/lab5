ffcfb5653580a317155a50c9b32a24148b6a3dba
SidorVl <112506210+SidorVl@users.noreply.github.com>
2022-10-22 21:53:02 +0300

    Add files via upload

---
+++ ConsoleApp4/DocumentWorker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    internal class DocumentWorker
+    {
+        public virtual void OpenDocument()
+        {
+            Console.WriteLine("Документ відкритий");
+        }
+        public virtual void EditDocument() 
+        {
+            Console.WriteLine("Правка документа доступна у версії Про");
+        }
+        public virtual void SaveDocument() 
+        {
+            Console.WriteLine("Збереження документа доступне у версії Про");
+        }
+    }
+}
---
+++ lab5/lab5/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Printer
+{
+    public void Print(string value)
+    {
+        Console.WriteLine(value);
+        if (this is Printer)
+        {
+            Console.BackgroundColor=ConsoleColor.Red;
+        }
+    }
+}
